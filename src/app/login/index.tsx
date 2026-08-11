186df15413f20fc7288f5a8d18e59982e7c8771c
Raphael-Aljona <raphael.aljona@gmail.com>
2026-08-11 10:00:35 -0300

    feat: Componentes do footer e botão criados

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,9 +1,21 @@
 import {View} from "react-native";
+import AuroraButton from "../../components/aurora_button/aurora_button";
+import {navigate} from "expo-router/build/global-state/routing";
+import {useRouter} from "expo-router";
+
+
 
 export default function Login(){
+
+    const router = useRouter();
+
+    function acessar() {
+        router.push("/listagem_item")
+    }
+
     return (
         <View>
-
+            <AuroraButton text="Pressione" onPress={acessar} />
         </View>
     )
 }
