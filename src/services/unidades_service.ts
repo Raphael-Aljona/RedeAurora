105beaa9cb0b4fe34ee481aba36bff6426d4683e
Vitor <ivitoror@gmail.com>
2026-08-14 10:02:03 -0300

    Consumo de itens para dashboard

--- src/services/unidades_service.ts
+++ src/services/unidades_service.ts
@@ -2,10 +2,19 @@ import {api} from "./api";
 
 export async function getAllUnidades() {
     try {
-        const response = await api.get('Unidade');
-
+        const response = await api.get('Item/quantidade-por-unidade');
         return response.data;
     } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getQtdItens() {
+    try {
+        const response = await api.get('Item');
+        return response.data;
+    }
+    catch (error) {
         console.error(error);
     }
 }
