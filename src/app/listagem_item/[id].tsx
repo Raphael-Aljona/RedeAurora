7d382e2038189e054c7515e927b24d406cc8dd28
Raphael-Aljona <raphael.aljona@gmail.com>
2026-09-02 08:40:03 -0300

    Commit to main pull

--- src/app/listagem_item/[id].tsx
+++ src/app/listagem_item/[id].tsx
@@ -1,47 +1,113 @@
-import React from "react";
+import React, {useState} from "react";
 import {FlatList, Image, Pressable, StyleSheet, Text, TextInput, View} from "react-native";
 import Card from "../../components/card/card";
 import useDetalhesSetor from "../../hooks/useDetalhesSetor";
 import CardUnidade from "../../components/card_unidade/card_unidade";
 import {Colors} from "../../constants/theme";
+import * as XLSX from "xlsx";
+import {File, Paths} from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import {ItemSetor} from "../../@types/setor";
+import {Ionicons} from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
 
 export default function ListagemItem() {
 
     const {itensSetor} = useDetalhesSetor();
+    const [itensFiltrados, setItensFiltrados] = useState<ItemSetor[]>([])
 
-    console.log(itensSetor);
+    function fetchItensFiltrados(busca?: string) {
+        const data = itensSetor.filter(value => value.nome_item.toString().includes(busca ?? ""));
+
+        setItensFiltrados(data);
+    }
+
+    async function exportarExcel(lista: any) {
+        try {
+            const worksheet = XLSX.utils.json_to_sheet(lista);
+
+            const workbook = XLSX.utils.book_new();
+
+            XLSX.utils.book_append_sheet(workbook, worksheet, "Patrimônios");
+
+            const base64 = XLSX.write(workbook, {
+                type: "base64",
+                bookType: "xlsx",
+            });
+
+            const file = new File(Paths.cache, "patrimônios.xlsx");
+
+            file.create({
+                overwrite: true,
+            });
+
+            file.write(base64, {
+                encoding: "base64",
+            });
+
+            if (await Sharing.isAvailableAsync()) {
+                await Sharing.shareAsync(file.uri, {
+                    mimeType:
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    dialogTitle: "Exportar patrimônios",
+                    UTI:
+                        "com.microsoft.excel.xlsx",
+                });
+            }
+        } catch (error) {
+            console.error("Erro ao exportar Excel:", error);
+        }
+    }
+
 
     return (
         <View style={estilos.Pagina}>
             <Text style={estilos.Titulo}>Lista de Patrimonio</Text>
             <Text style={estilos.Subtitulo}>Gerencie e visualize todos os ativos da empresa.</Text>
             <View style={estilos.ViewCaixaTexto}>
-                <Image style={estilos.ImagemCaixaTexto} source={require('../../../assets/imgs/lupa.png')} />
-                <TextInput placeholder="Buscar ativos..." style={estilos.CaixaDeTexto}/>
+                <Image style={estilos.ImagemCaixaTexto} source={require('../../../assets/imgs/lupa.png')}/>
+                <TextInput placeholder="Buscar ativos..." style={estilos.CaixaDeTexto} onChangeText={text => {
+                    fetchItensFiltrados(text)
+                    console.log(text)
+                    console.log(itensFiltrados)
+                    console.log("Filtrar");
+                }}/>
             </View>
-            <Pressable style={estilos.BotaoFiltrar}>
-                <Image style={estilos.ImagemFiltrar} source={require('../../../assets/imgs/filtro.png')} />
+            <Pressable style={estilos.BotaoFiltrar} onPress={}>
+                <Image style={estilos.ImagemFiltrar} source={require('../../../assets/imgs/filtro.png')}/>
                 <Text style={estilos.TextoFiltrar}>Filtros</Text>
             </Pressable>
-            <Pressable style={estilos.BotaoExportar}>
+            <Pressable style={estilos.BotaoExportar} onPress={event => {
+                exportarExcel(itensSetor);
+            }}>
                 <Image style={estilos.ImagemExportar} source={require('../../../assets/imgs/exportar.png')}/>
                 <Text style={estilos.TextoExportar}>Exportar Lista</Text>
             </Pressable>
-            <FlatList data={itensSetor}
-                      keyExtractor={(item) => item.id_item.toString()}
-                      renderItem={(item) =>
-                          <Card item={item.item} ></Card>}/>
+            {itensFiltrados.length > 0 ? <FlatList data={itensFiltrados}
+                                                   keyExtractor={(item) => item.id_item.toString()}
+                                                   renderItem={(item) =>
+                                                       <Card item={item.item}></Card>}/> :
+                <View style={estilos.filterError}>
+                    <View style={estilos.backgroundIcon}>
+                        <Ionicons name={"close"} size={50} color={Colors.marrom_escuro}></Ionicons>
+                    </View>
+                    <View style={estilos.divider}/>
+                    <Text style={estilos.textError}>Nenhum item encontrado</Text>
+                </View>}
+            {itensFiltrados.length > 0 && <View>
+
+                <View style={estilos.NavegacaoPagina}>
+                    <Pressable style={estilos.BotaoEsquerda}><Image style={estilos.ImagemDireita}
+                                                                    source={require('../../../assets/imgs/setaEsquerda.png')}/></Pressable>
+                    <Text style={estilos.TextoPagina}>Pagina 1 de 12</Text>
+                    <Pressable style={estilos.BotaoDireito}><Image style={estilos.ImagemDireita}
+                                                                   source={require('../../../assets/imgs/setaDireita.png')}/></Pressable>
+                </View>
+                <Pressable style={estilos.BotaoAdicionar}><Image style={estilos.ImagemDireita}
+                                                                 source={require('../../../assets/imgs/adicionar.png')}/></Pressable>
+            </View>}
 
 
-            <View style={estilos.NavegacaoPagina}>
-                <Pressable style={estilos.BotaoEsquerda}><Image style={estilos.ImagemEsquerda}
-                                                                source={require('../../../assets/imgs/setaEsquerda.png')}/></Pressable>
-                <Text style={estilos.TextoPagina}>Pagina 1 de 12</Text>
-                <Pressable style={estilos.BotaoDireito}><Image style={estilos.ImagemDireita}
-                                                               source={require('../../../assets/imgs/setaDireita.png')}/></Pressable>
-            </View>
-            <Pressable style={estilos.BotaoAdicionar}><Image style={estilos.ImagemAdicionar}
-                                                             source={require('../../../assets/imgs/adicionar.png')}/></Pressable>
         </View>
     )
 }
@@ -160,8 +226,6 @@ const estilos = StyleSheet.create({
         marginRight: 16
     },
 
-    ImagemEsquerda: {},
-
     TextoPagina: {
         fontSize: 14,
         color: "black"
@@ -192,5 +256,40 @@ const estilos = StyleSheet.create({
 
     },
 
-    ImagemAdicionar: {}
+    backgroundIcon: {
+        height: 70,
+        width: 70,
+        borderRadius: 1000,
+        backgroundColor: Colors.backgroundColor,
+        elevation: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    filterError: {
+        width: 350,
+        height: 210,
+        padding: 24,
+        backgroundColor: "white",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        marginBottom: 24,
+        elevation: 1,
+        gap: 10
+    },
+
+    textError: {
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+
+    divider: {
+        height: 1,
+        backgroundColor: '#E0E0E0',
+        width: '100%'
+    }
 })
